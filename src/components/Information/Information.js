18951d5ce9c059b986f17eb76c8ad8a3201465a5
khumayunkh <hasanovhumayun15@gmail.com>
2022-09-13 03:13:31 +0500

    style

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from './information.module.css'
 import info from './../../images/Group.svg'
+import Coints from './../../images/Coints.png'
 
 function Information(){
     return(
@@ -8,11 +9,14 @@ function Information(){
         <div className={style.container}>
             <div className={style.information}>
                 <div className={style.information_in}>
-                    <h2>
+                    <div className={style.title}>
+                        <h2>
                         Мы можем помочь вам, 
                         когда вам срочно нужны деньги до зарплаты.  
                         Достаточно зарегистрироваться
-                    </h2>
+                        </h2>
+                        <img src={Coints}/>
+                    </div>
                     <div className={style.logo}>
                         <img className={style.logo_in} src={info}/>
                     </div>
