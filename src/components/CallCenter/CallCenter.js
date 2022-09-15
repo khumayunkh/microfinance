8bac0536985b0cfdfd5a18d3c7752a7dfe2bca15
khumayunkh <hasanovhumayun15@gmail.com>
2022-09-15 17:46:58 +0500

    style

--- src/components/CallCenter/CallCenter.js
+++ src/components/CallCenter/CallCenter.js
@@ -5,6 +5,17 @@ import style from './callCenter.module.css'
 function CallCenter(){
     return(
         <>
+        <div className={style.container}>
+            <div className={style.callCenter}>
+                <h1>
+                    Возникли вопросы?
+                </h1>
+                <h2>
+                    Наши консультанты вам помогут, 
+                    просто позвоните — +7 (495) 115-50-95
+                </h2>
+            </div>
+        </div>
         </>
     )
 }
